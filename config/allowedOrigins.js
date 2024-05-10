4d90b8c8fa3d034265108b4f8c7f1353fc250f96
ROHIT VIJAYVARGIYA <66072894+rohitv007@users.noreply.github.com>
2024-05-11 02:07:22 +0530

    update: allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 
-const allowedOrigins = [process.env.CLIENT_URL];
+const allowedOrigins = [process.env.CLIENT_URL, process.env.WSS_CLIENT_URL];
 
 module.exports = allowedOrigins;
