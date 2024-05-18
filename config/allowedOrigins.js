b1a2a58a0361c146d28e8bbee8b6c66cfa02746e
Rohit Vijayvargiya <rohitvijayrv7@gmail.com>
2024-05-18 16:42:54 +0530

    update: ws url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const allowedOrigins = [process.env.HTTP_CLIENT_URL, process.env.WSS_CLIENT_URL, 'http://localhost:5173', 'ws://localhost:5173'];
+const allowedOrigins = [process.env.HTTP_CLIENT_URL, process.env.WS_CLIENT_URL, 'http://localhost:5173', 'ws://localhost:5173'];
 console.log(allowedOrigins);
 
 module.exports = allowedOrigins;
